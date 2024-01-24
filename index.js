74ab429f79f21f7d2d749930266cbf2682891da4
VikasMajoka <157447769+VikasMajoka@users.noreply.github.com>
2024-01-24 13:13:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let hour = 16
+if (hour >= 6 && hour <12 ){
+    console.log('Good Morning');}
+else if(hour >=12 && hour<18){
+    console.log('Good Afternoon');}
+    else
+    Console.log('Good Evening');
